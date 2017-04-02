c23112685f333d807410e26fc9fb31db00d53aea
479816822 <479816822@qq.com>
2017-04-02 17:11:06 +0800

    langren 1.注册、登陆、找回密码（电话、邮箱）、写游记、发表游记、首页、分页

---
+++ travel/src/com/cn/qd/travel/controller/LoginController.java
@@ -0,0 +1,136 @@
+package com.cn.qd.travel.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cn.qd.travel.entity.MDTravelNote;
+import com.cn.qd.travel.entity.MdUser;
+import com.cn.qd.travel.service.TravelService;
+import com.cn.qd.travel.service.UserService;
+import com.cn.qd.travel.util.ChangeIcon;
+import com.cn.qd.travel.util.Page;
+
+/**
+ * @author anmeihua 登录Controller
+ */
+@Controller
+public class LoginController {
+
+	@Autowired
+	UserService userService;
+	@Autowired
+	TravelService travelService;
+
+	/**
+	 * 登录页面
+	 */
+	@RequestMapping(value = "/login")
+	public String goLogin() {
+		return "login";
+	}
+
+	/**
+	 * 登录页面
+	 */
+	@RequestMapping(value = "/logins")
+	public String goLogins(Model model, MdUser user, HttpSession session, HttpServletRequest request,
+			HttpServletResponse response) {
+		// 判断是否自动登陆和是否已经登陆，只能登陆一次
+		MdUser loginUser = (MdUser) session.getAttribute("user");
+		Cookie[] cookie = request.getCookies();
+		String loginName = null;
+		String password = null;
+		String autoLogin = null;
+		ArrayList<Object> list=null;
+		autoLogin=(String)session.getAttribute("autoLogin");
+
+		for (int i = 0; i < cookie.length; i++) {
+			if (cookie[i].getName().equals("loginName")) {
+				loginName = cookie[i].getValue();
+			} else if (cookie[i].getName().equals("password")) {
+				password = cookie[i].getValue();
+			} 
+		}
+		
+		if (loginUser != null) {// 同一个浏览器登陆
+			 list = userService.oneListResultProvider(loginUser, null);
+		} else {// 未登陆
+			if (loginName != null && password != null) {
+				if("autoLogin".equals(autoLogin)){
+					MdUser loginAutoUser=new MdUser();
+					loginAutoUser.setMdTelephone(loginName);
+					loginAutoUser.setMdPassword(password);
+					list = userService.oneListResultProvider(loginAutoUser, null);
+				}else{//不登陆
+					return "redirect:/to_index";
+				}
+			}else if(user!=null){//主动登陆
+				list = userService.oneListResultProvider(user, null);
+			}else{//不登陆
+				return "redirect:/to_index";
+			}
+		}
+
+		
+		if(list.size()==1){//登陆成功
+//			if("已登陆".equals(((MdUser)list.get(0)).getMdStdname())){
+//				model.addAttribute("user", ((MdUser)list.get(0)).getMdStdname());
+//				return "redirect:/login";
+//			}
+			
+			((MdUser)list.get(0)).setMdStdname("已登陆");
+//			int i=userService.update(list.get(0), null);
+			
+			//用户头像转换
+			String savePath = request.getSession().getServletContext().getRealPath("upload");
+			String userHeadImg=ChangeIcon.changeImg(((MdUser)list.get(0)).getMdIcon(),savePath);
+			((MdUser)list.get(0)).setUserHeadImg(userHeadImg);
+			
+			Cookie loginNameCookie=new Cookie("loginName", user.getMdTelephone());
+			Cookie passwordCookie=new Cookie("password", user.getMdPassword());
+			Cookie autoLoginCookie=new Cookie("autoLogin", "autoLogin");
+			loginNameCookie.setMaxAge(1000*10000);
+			passwordCookie.setMaxAge(1000*10000);
+			autoLoginCookie.setMaxAge(1000*10000);
+			response.addCookie(passwordCookie);
+			response.addCookie(loginNameCookie);
+			session.setAttribute("user", (MdUser)list.get(0));
+			session.setAttribute("autoLogin", autoLoginCookie);
+			return "redirect:/to_index";
+		}else{
+			return "redirect:/login";
+		}
+	}
+	
+	/**
+	 * 跳转到网站首页
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/to_index")
+	public String to_index(Model model,HttpServletRequest request){
+		//查询游记分页显示
+		ArrayList<Object> travelObject=travelService.oneListResultProvider(null,null);
+		ArrayList<MDTravelNote> travelList=new ArrayList<MDTravelNote>();
+		for (Object object : travelObject) {
+			travelList.add((MDTravelNote)object);
+		}
+		Page page=travelService.getPage();
+		model.addAttribute("page", page);
+		model.addAttribute("travelList", travelList);
+		return "index";
+	}
+
+
+}
---
+++ travel/src/com/cn/qd/travel/controller/RegisterController.java
@@ -0,0 +1,107 @@
+package com.cn.qd.travel.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cn.qd.travel.entity.MdUser;
+import com.cn.qd.travel.service.UserService;
+import com.cn.qd.travel.util.ChangeIcon;
+import com.cn.qd.travel.util.GUID;
+
+/**
+ * @author anmeihua 用户注册 Controller
+ */
+@Controller
+@RequestMapping(value = "/alu")
+public class RegisterController {
+
+	@Autowired
+	UserService userService;
+
+	/**
+	 * 检查手机号是否存在
+	 * 
+	 */
+	@RequestMapping(value = "/checkPhone")
+	@ResponseBody
+	public int checkPhone(String mdTelephone, Model model) {
+		int ret = userService.checkPhone(mdTelephone);
+		model.addAttribute("userPhone", mdTelephone);
+		return ret;
+	}
+
+	/**
+	 * 注册页面1
+	 * 
+	 */
+	@RequestMapping(value = "/regist")
+	public String goRegis() {
+		return "register1";
+	}
+
+	/**
+	 * 到注册页面2
+	 * 
+	 */
+	@RequestMapping(value = "/regists", method = { RequestMethod.POST })
+	public String goRegis1(Model model, MdUser user) {
+		model.addAttribute("mdTelephone", user.getMdTelephone());
+		return "register2";
+	}
+
+
+	/**
+	 * 注册提交 注册成功转发到首页 注册失败重定向到注册2页面
+	 */
+	@RequestMapping("/register2")
+	public String submit(Model model, MdUser user,HttpServletRequest request) {
+		String savePath = request.getSession().getServletContext().getRealPath("upload");
+		ChangeIcon chIcon=new ChangeIcon();
+		byte[] pic=chIcon.getIcon(savePath);
+		user.setMdUserRecid(GUID.createGuid());
+		user.setMdSex("男");
+		user.setMdCreateTime(new Date());
+		user.setMdAge((short)18);
+		user.setMdIsVip("否");
+		user.setMdLevel("1");
+		user.setMdPraise((long)0);
+		user.setMdSignature("青春是一辈子的财富！！！");
+		user.setMdIcon(pic);
+		int ret = userService.insertSelective(user);
+		if (ret > 0) {
+			model.addAttribute("registMsg", "注册成功！");
+			return "login";
+		} else {
+			model.addAttribute("registMsg", "网络异常，请重试！");
+			return "register2";
+		}
+	}
+ 
+	// 验证验证码
+	@ResponseBody
+	@RequestMapping(value = "checkCode", method = { RequestMethod.POST })
+	public Map<String, String> addUser(@RequestBody MdUser user, HttpSession session) {
+		String code1 = user.getCode();
+		String code2 = session.getAttribute("code").toString();
+		Map<String, String> result = new HashMap<String, String>();
+		if (!code1.equalsIgnoreCase(code2)) { // 忽略验证码大小写
+			return null;
+		} else {
+			result.put("success", "success");
+			return result;
+		}
+	}
+
+}
